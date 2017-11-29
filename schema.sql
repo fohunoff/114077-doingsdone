29b27927c81f1ff2a63f477cb3fa3bb7663f5f30
Ivan Zagainov <fohunoff@gmail.com>
2017-11-29 23:20:13 +0300

    Исправил значение полей в базе данных

--- schema.sql
+++ schema.sql
@@ -5,11 +5,11 @@ USE doingsdone;
 CREATE TABLE users (
     id              int AUTO_INCREMENT PRIMARY KEY,
     email           varchar(128) NOT NULL,
-    password        varchar(32) NOT NULL,
+    password        varchar(60) NOT NULL,
     name            varchar(64) NOT NULL,
     avatar_path     varchar(128),
     group_id        int(5),
-    is_deleted      tinyint(1)
+    is_deleted      bit(1)
 );
 
 CREATE UNIQUE INDEX email ON users(email);
@@ -33,7 +33,7 @@ CREATE TABLE tasks (
     date                date,
     user_id             int(10) NOT NULL,
     project_id          int(10) NOT NULL,
-    is_done             tinyint(1),
+    is_done             bit(1),
     file_path           varchar(128)
 );
 
